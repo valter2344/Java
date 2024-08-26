86e6d8a948e33c84836b2e9df5b9013df2962a6e
valter2344 <valter.lonnqvist65@gmail.com>
2024-08-26 12:54:15 +0300

    Add files via upload

---
+++ java/newJavaFile.java
@@ -0,0 +1,26 @@
+
+public class newJavaFile {
+
+    
+    public static void main(String args[]) {
+       int myInt = 10;
+       double myDouble = myInt;
+       double myFloat = myInt; 
+       
+       System.out.println(myInt);
+       System.out.println(myFloat / 2);
+       System.out.println(myDouble / 9);
+       */
+        
+       double myDouble = 9.7888889;
+       int myInt = (int)myDouble;    
+       
+       */
+       
+       // Example 2
+       
+       // Int Score variables 
+       
+       int Score variable        
+    }
+}
